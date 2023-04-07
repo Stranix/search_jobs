5ecf4176097ea6c36f463450885efdd64dcd77b6
Stranix <Phantom2525@gmail.com>
2023-04-07 10:47:36 +0300

    REFACTOR parse func

--- parsers/hh_api_parser.py
+++ parsers/hh_api_parser.py
@@ -1,19 +1,21 @@
 import schemas
 
 
-def parse_hh_response_vacancy_salary(vacancy: dict) -> schemas.Salary | None:
+def parse_hh_response_vacancy(vacancy: dict) -> schemas.Vacancy:
+    salary = None
+
     if vacancy['salary']:
-        return schemas.Salary(
+        salary = schemas.Salary(
             s_from=vacancy['salary']['from'],
             to=vacancy['salary']['to'],
             currency=vacancy['salary']['currency'],
             gross=vacancy['salary']['gross']
         )
 
-
-def parse_hh_response_vacancy(vacancy: dict) -> schemas.Vacancy:
-    return schemas.Vacancy(
+    vacancy = schemas.Vacancy(
         id=vacancy['id'],
         name=vacancy['name'],
-        salary=parse_hh_response_vacancy_salary(vacancy),
+        salary=salary,
     )
+
+    return vacancy
--- parsers/super_job_api_parser.py
+++ parsers/super_job_api_parser.py
@@ -1,19 +1,21 @@
 import schemas
 
 
-def parse_sj_response_vacancy_salary(vacancy: dict) -> schemas.Salary | None:
+def parse_sj_response_vacancy(vacancy: dict) -> schemas.Vacancy:
+    salary = None
+
     if vacancy['payment_from'] or vacancy['payment_to']:
-        return schemas.Salary(
+        salary = schemas.Salary(
             s_from=vacancy['payment_from'],
             to=vacancy['payment_to'],
             currency=vacancy['currency'],
             gross=False
         )
 
-
-def parse_sj_response_vacancy(vacancy: dict) -> schemas.Vacancy:
-    return schemas.Vacancy(
+    vacancy = schemas.Vacancy(
         id=vacancy['id'],
         name=vacancy['profession'],
-        salary=parse_sj_response_vacancy_salary(vacancy),
+        salary=salary,
     )
+
+    return vacancy
